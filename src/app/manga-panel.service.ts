f524cdb9282fda6d7b35365b3496fa892066abe2
iOllaph <pabllo164164@gmail.com>
2023-04-16 17:50:35 -0300

    Applyed the logic

--- src/app/manga-panel.service.ts
+++ src/app/manga-panel.service.ts
@@ -1,9 +1,71 @@
 import { Injectable } from '@angular/core';
 
+
+
+export interface Manga {
+  title: string;
+  chapter: string;
+  image: string;
+}
+
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class MangaPanelService {
 
+  favoritePanels: Manga[] = [];
+  panels: Manga[] = [
+
+    {
+
+      title: "Jujutsu Kaisen",
+      chapter: "24",
+      image: "../assets/home-assets/sun-ken-rock.jpg"
+
+    },
+    {
+
+      title: "Sun Ken Rock",
+      chapter: "32",
+      image: "../assets/home-assets/sun-ken-rock.jpg"
+
+    },
+    {
+
+      title: "Tokyo Ghoul",
+      chapter: "23",
+      image: "../assets/home-assets/tokyo-ghoul-1.jpg"
+
+    }
+
+  ];
+
   constructor() { }
+
+  addToFavoriteArray(manga: Manga) {
+
+    this.favoritePanels.push(manga);
+
+  }
+
+  getFavoritePanels() {
+
+    return this.favoritePanels;
+
+  }
+
+  addToPanelsArray(manga: Manga): void {
+
+    this.panels.push(manga);
+
+  }
+
+  getPanelsArray() {
+
+    return this.panels;
+
+  }
+
 }
