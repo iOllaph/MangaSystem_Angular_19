b0af28f7bae63a79b3a23c66890d1d60db07b3f5
iOllaph <pabllo164164@gmail.com>
2023-04-16 17:53:34 -0300

    Added the logic

--- src/app/favorite-manga/favorite-manga.component.ts
+++ src/app/favorite-manga/favorite-manga.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MangaPanelService, Manga } from './../manga-panel.service';
 
 @Component({
   selector: 'app-favorite-manga',
@@ -7,4 +8,44 @@ import { Component } from '@angular/core';
 })
 export class FavoriteMangaComponent {
 
+  addedFavoritePanels: Manga[] = [];
+
+  constructor(private mangaPanelService: MangaPanelService) { };
+
+  ngOnInit(): void {
+
+    this.addedFavoritePanels = this.mangaPanelService.getFavoritePanels();
+
+  }
+
+  checkPanels() {
+
+    if (this.addedFavoritePanels.length > 0) {
+
+      return "none";
+
+    } else {
+
+      return "flex";
+    }
+
+  }
+
+
+  checkIndexValue(index: number) {
+
+    return Number.isInteger(index / 5);
+
+  }
+
+
+  removeMangaPanel(index: any) {
+
+    if (index >= 0 && index < this.addedFavoritePanels.length) {
+
+      // Remove from mangaListPanels array
+      this.addedFavoritePanels.splice(index, 1)[0];
+
+    }
+  }
 }
--- src/app/manga-list/manga-list.component.ts
+++ src/app/manga-list/manga-list.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { MangaPanelService, Manga } from './../manga-panel.service';
+
+
 
 @Component({
   selector: 'app-manga-list',
@@ -6,5 +9,90 @@ import { Component } from '@angular/core';
   styleUrls: ['./manga-list.component.scss']
 })
 export class MangaListComponent {
+
+  disableButton = false;
+
+  favoritePanels: Manga[] = [];
+
+  removedPanels: Manga[] = [];
+
+  mangaListPanels: Manga[] = [];
+
+  servicePanels: Manga[] = [];
+
+  existingPanels: Manga[] = [];
+
+  constructor(private mangaPanelService: MangaPanelService) { }
+
+  ngOnInit(): void {
+
+    this.servicePanels = this.mangaPanelService.getPanelsArray();
+    this.favoritePanels = this.mangaPanelService.getFavoritePanels();
+    this.populateMangaListPanels();
+
+  }
+
+  checkPanels() {
+
+    if (this.mangaListPanels.length > 0) {
+
+      return "none";
+
+    } else {
+
+      return "flex";
+    }
+
+  }
+
+
+  populateMangaListPanels(): void {
+
+    this.mangaListPanels = this.existingPanels.concat(this.servicePanels);
+  }
+
+  checkIndexValue(index: number) {
+
+    return Number.isInteger(index / 5);
+
+  }
+
+  removeMangaPanel(index: any) {
+
+    if (index >= 0 && index < this.mangaListPanels.length) {
+
+      // Remove from mangaListPanels array
+      const removedManga = this.mangaListPanels.splice(index, 1)[0];
+
+      // Find the manga panel in existingPanels array and remove it
+      const existingPanelIndex = this.existingPanels.findIndex(panel => panel === removedManga);
+      if (existingPanelIndex !== -1) {
+        this.existingPanels.splice(existingPanelIndex, 1);
+      }
+
+      const servicePanelIndex = this.servicePanels.findIndex(panel => panel === removedManga);
+      if (servicePanelIndex !== -1) {
+        this.servicePanels.splice(servicePanelIndex, 1);
+      }
+
+      const favoritePanelsIndex = this.favoritePanels.findIndex(panel => panel === removedManga);
+      if (favoritePanelsIndex !== -1) {
+        this.favoritePanels.splice(favoritePanelsIndex, 1);
+      }
+
+    }
+
+  }
+
+  addFavoriteMangaPanel(manga: any) {
+
+    this.mangaPanelService.addToFavoriteArray(manga);
+
+  }
+
+  isFavorite(manga: Manga): boolean {
+
+    return this.favoritePanels.includes(manga);
+  }
 
 }
